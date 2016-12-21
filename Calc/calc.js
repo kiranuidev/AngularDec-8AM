e6e8f3499056d4260c22a75ec1b74cec6fc1b467
narasimha kbnd <narasimha kbnd>
2016-12-21 09:12:23 +0530

    Added Code:

--- Calc/calc.js
+++ Calc/calc.js
@@ -2,21 +2,5 @@ var first = document.getElementById("txtFirstNumber");
 var second = document.getElementById("txtSecondNumber");
 var result =  document.getElementById("txtResult");
 //
-function add(){
-var total = parseInt(first.value)+parseInt(second.value);
-result.value= total;
-}
 
-function sub(){
-var total = parseInt(first.value)-parseInt(second.value);
-result.value= total;
-}
 
-function multiply(){
-var total = parseInt(first.value)*parseInt(second.value);
-result.value= total;
-}
-function divide(){
-var total = parseInt(first.value)/parseInt(second.value);
-result.value= total;
-}
