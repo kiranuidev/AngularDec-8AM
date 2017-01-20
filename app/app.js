f7beb78e96c98deeba09bdcbd8c47c7433ee27a6
narasimha kbnd <narasimha kbnd>
2017-01-20 09:14:49 +0530

    added code

---
+++ app/app.js
@@ -0,0 +1,9 @@
+(function(){
+    
+    //module creation
+    //first param: Module Name
+    //second params: module names injected.
+    angular.module("goodServices",["login"]);
+    
+    
+})();
