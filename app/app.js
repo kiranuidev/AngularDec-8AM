0c64b2b0cbc9e8856a7213b6d8851fe339d423ba
narasimha kbnd <narasimha kbnd>
2017-01-21 09:25:39 +0530

    added modules and configuration and run function

--- app/app.js
+++ app/app.js
@@ -3,7 +3,29 @@
     //module creation
     //first param: Module Name
     //second params: module names injected.
-    angular.module("goodServices",["login"]);
+    angular.module("goodServices",["login","register"]);
     
+   //consuming the module
+    angular.module("goodServices")
+    .config(function(initProvider){
+        console.log("I m the good services");
+    })
+    .run(function(){
+        console.log("I m the good services run function");
+    })
+    .provider("init",function(){
+        console.log("provider-good");
+        this.$get=function(){
+            
+        };
+    });
     
 })();
+
+
+
+
+
+
+
+
