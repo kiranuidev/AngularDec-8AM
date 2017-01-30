4ce9323c2765c4d91d0b7bf3e7ed672d41023e65
narasimha kbnd <narasimha kbnd>
2017-01-30 09:14:27 +0530

    added directives

---
+++ app/common/commonBrandName.js
@@ -0,0 +1,27 @@
+(function(){
+     function brandNameFn(){
+         return{
+           template:' <a class="navbar-brand" href="#">{{hc.brandName}}</a>'
+         };
+     }
+    
+    angular.module("common")
+    .directive("cmBrandName",[brandNameFn]);
+    
+    /* directive definition
+    
+    directiveName:"cmBrandName"
+    onTheHtml :<cm-brand-name></cm-brand-name>
+    
+    
+    
+    
+    */
+})();
+
+
+
+
+
+
+
