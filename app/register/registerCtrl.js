9e383840af410ef5b1e32aa58fa763659c5a3c98
narasimha kbnd <narasimha kbnd>
2017-02-03 09:15:19 +0530

    added code

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -1,53 +1,46 @@
-(function(){
-    
-    function registerCtrl(lookupSvc){
+(function () {
+
+    function registerCtrl(lookupSvc) {
         var vm = this;
-        vm.user={
-            firstName:""
+        vm.user = {
+            firstName: ""
         };
-        vm.register=function(){
+        vm.register = function () {
             console.log(vm.user);
         }
-       // vm.countries = lookupSvc.getCountries();
-        
+        vm.DobRange = {
+           maxdate:"-18Y"
+        };
+        vm.DateofReservationRange = {
+            mindate: "0D",
+            maxdate: "+1M"
+        };
+        // vm.countries = lookupSvc.getCountries();
+
         lookupSvc.getCountries()
-        .then( function(res){
-            console.log(res);
-        })
-        .catch(function(err){
-            console.log(err);
-        })
+            .then(function (res) {
+                console.log(res);
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
         vm.selectedCountry = lookupSvc.defaultCountry();
-       /* angular.forEach( vm.countries, function(item){
-           if(item.code=="IN"){
-               //vm.selectedCountry=item;
-           }
-        })*/
+        /* angular.forEach( vm.countries, function(item){
+            if(item.code=="IN"){
+                //vm.selectedCountry=item;
+            }
+         })*/
         lookupSvc.getCountriesFromJson()
-        .then( function(res){
-            console.log(res);
-            vm.countries= res;
-        })
-        .catch(function(err){
-            console.log(err);
-        })
-        
+            .then(function (res) {
+                console.log(res);
+                vm.countries = res;
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+
     }
     angular.module("register")
-    .controller("registerCtrl",["lookupService",registerCtrl]);
-    
+        .controller("registerCtrl", ["lookupService", registerCtrl]);
+
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
