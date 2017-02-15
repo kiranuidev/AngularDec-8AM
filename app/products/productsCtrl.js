f00564d380c3b1d15236cb6e946b6ac6c2026394
narasimha kbnd <narasimha kbnd>
2017-02-15 09:03:59 +0530

    added code

--- app/products/productsCtrl.js
+++ app/products/productsCtrl.js
@@ -9,8 +9,20 @@
         })
         .catch(function(err){
             console.log(err);
-        })
-        
+        });
+        vm.minimumProducts=10;
+        vm.showMore = function(){
+          vm.minimumProducts+=10;  
+        }; 
+        vm.orderByCrietria="Price";
+        vm.orderByPrice = function(){
+            if( vm.orderByCrietria=="Price"){
+                vm.orderByCrietria="-Price";
+            }
+            else{
+                 vm.orderByCrietria="Price";
+            }
+        }
     }
     angular.module("products")
     .controller("productsCtrl",["productsSvc",productsCtrl]);
